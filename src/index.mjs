a46cc719bddf064adbd737df225aae290bcf9c8a
kura <91504106+kura54@users.noreply.github.com>
2023-11-08 06:04:48 +0000

    Update 4 files

--- src/index.mjs
+++ src/index.mjs
@@ -2,9 +2,16 @@
  * const, letの変数宣言
  */
 
- var val1 = 'var変数'
- console.log(val1);
+// var val1 = "var変数";
+// console.log(val1);
 
- //var変数は上書き可能
-val1 = "var変数を上書き";
-console.log(val1);
+// //var変数は上書き可能
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+const val4 = {
+  name: "takoyaki",
+  age: 28,
+};
+val4.adress = "Hiroshima";
+console.log(val4);
